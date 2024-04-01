b79292b096d837fe2c33ba0f7e44b9f1871b7b0f
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2024-04-01 16:05:27 +0500

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-var userbtn = document.querySelector('.user__btn');
-var logList = document.querySelector('.log__list');
-userbtn.addEventListener('click', ()=>{
-logList.classList.toggle('log__list__open')
-});
